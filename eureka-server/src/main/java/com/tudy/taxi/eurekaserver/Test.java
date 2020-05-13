dfcb04a3d94180063cec308b98f70464da19e104
jokerdavid <415559298@qq.com>
2020-05-13 15:58:54 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/tudy/taxi/eurekaserver/Test.java
@@ -0,0 +1,11 @@
+package com.tudy.taxi.eurekaserver;
+
+/**
+ * @author liulei
+ * @date 2020/5/13 3:53 下午
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("真特么不容易啊！！！");
+    }
+}
