d17e907733d6e0258c045f39cc091a3be678edd9
jokerdavid <415559298@qq.com>
2020-05-13 16:06:31 +0800

    Test类新增打印语句2

--- eureka-server/src/main/java/com/tudy/taxi/eurekaserver/Test.java
+++ eureka-server/src/main/java/com/tudy/taxi/eurekaserver/Test.java
@@ -8,5 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("真特么不容易啊！！！");
         System.out.println("yes");
+        System.out.println("谁说不是呢");
+
     }
 }
